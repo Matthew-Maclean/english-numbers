598c27942cbae45c1c6206d145d5febec2bc0522
Matthew Maclean <Matthew-Maclean@users.noreply.github.com>
2017-04-22 19:09:59 -0300

    added comment docs

--- src/formatting.rs
+++ src/formatting.rs
@@ -1,15 +1,24 @@
+/// Formatting options for conversion
+///
+/// This struct does not control whether "short" or "long" number formats are used.
 #[derive(Copy, Clone)]
 pub struct Formatting
 {
+    /// Controls the casing. "One Hundred" vs "one hundred"
     pub title_case: bool,
+    /// Controls the use of spaces. "One Hundred" vs "OneHundred"
     pub spaces: bool,
+    /// Controls the use of "and". "One Hundred and One" vs "One Hundred One"
     pub conjunctions: bool,
+    /// Controls the use of commas. "One Thousand, One Hundred" vs "One Thousand One Hundred"
     pub commas: bool,
+    /// Controls the use of dashes. "Forty-Five" vs "Forty Five"
     pub dashes: bool,
 }
 
 impl Formatting
 {
+    /// All formatting options enabled
     pub fn all() -> Formatting
     {
         Formatting
@@ -22,6 +31,7 @@ impl Formatting
         }
     }
 
+    /// No formatting options enabled
     pub fn none() -> Formatting
     {
         Formatting
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-#![allow(dead_code)]
+//! # english-numbers
+//!
+//! A library for converting integer to their written-english formats.  
+//! Supports both American "short" and European "long" number formats.
 
 mod groups;
 mod hundreds;
@@ -12,6 +15,11 @@ mod test;
 
 pub use formatting::Formatting;
 
+/// Converts a number to it's written format, using "short" format.
+///
+/// Arguments:
+/// * `val`: the `i64` to convert  
+/// * `fmt`: the formatting options to use
 pub fn convert(val: i64, fmt: Formatting) -> String
 {
     groups::Groups::new(val)
@@ -19,6 +27,10 @@ pub fn convert(val: i64, fmt: Formatting) -> String
         .build(fmt)
 }
 
+/// Converts a number to it's written format, using "short" format with all formatting options enabled.
+///
+/// # Arguments:
+/// * `val` - the `i64` to convert
 pub fn convert_all_fmt(val: i64) -> String
 {
     groups::Groups::new(val)
@@ -26,6 +38,10 @@ pub fn convert_all_fmt(val: i64) -> String
         .build(Formatting::all())
 }
 
+/// Converts a number to it's written format, using "short" format with no formatting options enabled.
+///
+/// # Arguments:
+/// * `val` - the `i64` to convert
 pub fn convert_no_fmt(val: i64) -> String
 {
     groups::Groups::new(val)
@@ -33,6 +49,11 @@ pub fn convert_no_fmt(val: i64) -> String
         .build(Formatting::none())
 }
 
+/// Converts a number to it's written format, using "long" format.
+///
+/// # Arguments:
+/// * `val` - the `i64` to convert  
+/// * `fmt` - the formatting options to use
 pub fn convert_long(val: i64, fmt: Formatting) -> String
 {
     groups::Groups::new(val)
