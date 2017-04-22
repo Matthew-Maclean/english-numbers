322db0b9fa6375fc163aaa5ab6c4e60129df2018
Matthew Maclean <myself123@live.com>
2017-04-22 12:08:04 -0300

    spelling

--- src/tens.rs
+++ src/tens.rs
@@ -51,7 +51,7 @@ impl Tens
             {
                 2 => "twenty",
                 3 => "thirty",
-                4 => "fourty",
+                4 => "forty",
                 5 => "fifty",
                 6 => "sixty",
                 7 => "seventy",
